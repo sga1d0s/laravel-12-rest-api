0f1942846b0d9f8647602155ffd832f93bf8a759
Sergio Galdos <galdos.sergio@outlook.com>
2025-03-23 13:37:51 +0100

    [dev] [fix] api routes

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        // API Routes
+        Route::middleware('api')
+            ->prefix('api')
+            ->group(base_path('routes/api.php'));
+
+        // Web Routes
+        Route::middleware('web')
+            ->group(base_path('routes/web.php'));
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\PotionController;
 use App\Http\Controllers\IngredientController;
